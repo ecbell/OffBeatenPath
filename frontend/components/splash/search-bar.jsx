eb34c9078747f75b143891e9986d1ef6cc26710f
ecbell <ebell691@gmail.com>
2021-11-22 16:35:56 -0600

    stopping place to update ruby

--- frontend/components/splash/search-bar.jsx
+++ frontend/components/splash/search-bar.jsx
@@ -4,9 +4,12 @@ const searchBar = () => {
   return (
   <div>
     <form>
-
-
-
+      <input
+        type='text'
+        className='search-bar'
+        placeholder='Search by city, park, or trail name'
+      />
+        <input className='search-btn' type='submit' value='search' />
     </form>
   </div>
   )
