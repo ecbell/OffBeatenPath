56b638be18016e2fe15a1902acfb827f1b3e044a
ecbell <ebell691@gmail.com>
2021-11-23 17:47:36 -0600

    add seed data

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :show, :edit, :update]
+    resources :trails, only: [:index, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,42 @@
 User.destroy_all
 demo_user = User.create([{first_name: "demo-user", last_name: "user", email: "demo@demo.com", city: "", state: "", password: "password"}])
 
+trail1 = Trail.create([{trail_name: "Angels Landing Trail",
+                        park_id: 1,
+                        difficulty: "hard",
+                        length: 4.4,
+                        elevation_gain: 1604,
+                        route_type: "Out & Back",
+                        description: "The Angels Landing Trail is a strenuous
+                                      route in Zion National Park with steep 
+                                      drop-offs and very narrow sections. 
+                                      The technical route and incredible views
+                                      of Zion Canyon make this hike the most
+                                      popular in Zion.",
+                        lng: -112.95142,
+                        lat: 37.25976 }])
 
+trail2 = Trail.create([{trail_name: "Zion Narrows Riverside Walk",
+                        park_id: 1,
+                        difficulty: "easy",
+                        length: 1.9,
+                        elevation_gain: 193,
+                        route_type: "Out & Back",
+                        description: "The Zion Narrows Riverside Walk is a
+                                      1.9 mile heavily trafficked out and back
+                                      trail located near Springdale, Utah
+                                      that features a waterfall and is good for
+                                      all skill levels. The trail is primarily
+                                      used for hiking, walking, running, and
+                                      nature trips and is best used from March
+                                      until October. The Narrows should be on
+                                      every outdoor enthusiast's bucket list.
+                                      This route along the Riverside Walk Trail 
+                                      is a relatively easy and paved
+                                      out and back located at the end of the
+                                      Zion Canyon Scenic Drive.",    
+                        lng: -112.94829,
+                        lat: 37.293665}])
 
 #   id: 3,
 #   first_name: "Henry David",
