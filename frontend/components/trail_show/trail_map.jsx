7562f40e82c0058c2b2effba3ed801781c90192e
ecbell <ebell691@gmail.com>
2021-11-25 00:17:18 -0600

    start mapbox functionality

--- frontend/components/trail_show/trail_map.jsx
+++ frontend/components/trail_show/trail_map.jsx
@@ -7,21 +7,58 @@ export default class TrailMap extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      lng: -70.9,
-      lat: 42.35,
-      zoom: 9
+      lng: -112.95122,
+      lat: 37.25909,
+      zoom: 12
     };
     this.mapContainer = React.createRef();
   }
+
+  // Way to get the coordinates for the angel trail landing.
+  // https://api.mapbox.com/directions/v5/mapbox/walking/-112.95122,37.25909;-112.94787,37.26931?geometries=geojson&access_token=
+// api request for retrieving directions. GET /directions/v5 / { profile } / { coordinates }
+
+
+
+  // Boiler Plate code from mapbox to render map
   componentDidMount() {
     const { lng, lat, zoom } = this.state;
     const map = new mapboxgl.Map({
       container: this.mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
+      style: 'mapbox://styles/ebell691/ckwe9s1za164315qxq1mediw9',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
     });
 
+
+    // make an ajax request to api to get JSON coordinates of trail
+    const url = 'https://api.mapbox.com/directions/v5/mapbox/walking/-112.95122,37.25909;-112.94787,37.26931?geometries=geojson&access_token=
+    const getTrailCoordinates = () => 
+      $.ajax({
+      method: "GET",
+      url: url
+      })
+
+      // code from tutorial on mapbox
+      const data = json.routes[0];
+      const route = data.geometry.coordinates;
+      const geojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: route
+        }
+      };
+
+      // getRoute => 
+    map.on('load', () => {
+      // make an initial directions request that
+      // starts and ends at the same location
+      getRoute(start);
+    })
+
+    // set new state when user moves map
     map.on('move', () => {
       this.setState({
         lng: map.getCenter().lng.toFixed(4),
@@ -30,6 +67,14 @@ export default class TrailMap extends React.PureComponent {
       });
     });
   }
+
+  // set marker 
+    // let marker = () => {
+    //   new mapboxgl.Marker()
+    //   .setLngLat([-112.95122, 37.25909])
+    //   .addTo(map)}
+  
+
 
   render() {
     const { lng, lat, zoom } = this.state;
