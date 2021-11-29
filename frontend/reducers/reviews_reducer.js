42b87c9349193241bf54cacb8a52aed2a1821839
ecbell <ebell691@gmail.com>
2021-11-29 16:57:57 -0600

    start frontend show components for reviews

---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,26 @@
+import { 
+  RECEIVE_REVIEWS,
+  RECEIVE_REVIEW,
+  REMOVE_REVIEW 
+} from '../actions/review_actions'
+
+
+const ReviewsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let nextState = Object.assign({}, oldState)
+
+  switch (action.type) {
+    case RECEIVE_REVIEWS:
+      return action.reviews
+    case RECEIVE_REVIEW:
+      nextState[action.review.id] = action.review
+      return nextState;
+    case REMOVE_REVIEW:
+      delete nextState[action.reviewId]
+      return nextState;
+    default:
+      return oldState;
+  }
+}
+
+export default ReviewsReducer;
