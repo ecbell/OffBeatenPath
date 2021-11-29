47b151b944c7b33df4007923158ff321d1abbd83
ecbell <ebell691@gmail.com>
2021-11-29 16:28:04 -0600

    finish review actions

---
+++ frontend/actions/review_actions.js
@@ -0,0 +1,62 @@
+import * as APIUtil from '../util/review_api_util'
+
+
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const RECEIVE_REVIEWS = 'RECEIVE_REVIEWS';
+export const REMOVE_REVIEW = 'REMOVE_REVIEW'
+
+const receiveReviews = (reviews) => {
+  return {
+    type: RECEIVE_REVIEWS,
+    reviews
+  }
+}
+
+export const requestReviews = () => {
+  return dispatch => {
+    return APIUtil.fetchReviews()
+      .then((reviews) => dispatch(receiveReviews(reviews)))
+  }
+}
+
+const receiveReview = (review) => {
+  return {
+    type: RECEIVE_REVIEW,
+    review
+  }
+}
+
+export const requestReview = (reviewId) => {
+  return dispatch => {
+    return APIUtil.fetchReview(reviewId)
+      .then((review) => dispatch(receiveReview(review)))
+  }
+}
+
+export const createReview = (review) => {
+  return dispatch => {
+    return APIUtil.createReview(review)
+      .then((review) => dispatch(receiveReview(review)))
+  }
+}
+
+export const updateReview = (review) => {
+  return dispatch => {
+    return APIUtil.updateReview(review)
+      .then((review) => dispatch(receiveReview(review)))
+  }
+}
+
+const removeReview = (reviewId) => {
+  return {
+    type: REMOVE_REVIEW,
+    reviewId
+  }
+}
+
+export const deleteReview = (reviewId) => {
+  return dispatch => {
+    return APIUtil.deleteReview(reviewId)
+      .then((review) => dispatch(removeReview(review.id)))
+  }
+}
