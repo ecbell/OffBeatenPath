f68c54dc8d63901016b1b987500d490b2e747b7d
ecbell <ebell691@gmail.com>
2021-12-01 00:00:42 -0600

    stopping point for the night

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -38,20 +38,40 @@ class ReviewForm extends React.Component {
   
     return (
       <form className='form-container' onSubmit={this.handleSubmit}>
-        {/* <h1>Write Review </h1> */}
-        <label>Rating
+        <h1 className='create-form-title'> Review this Trail! </h1>
+        <label>
           {/* <input type='text' value={this.state.star_rating} onChange={this.update('star_rating')} /> */}
-          {/* <input type='radio' name='rating' value='1' onClick={this.update('star_rating')}/> */}
-          <fas className='star' size={100}/>
+          <input type='radio' name='rating' value='1' onClick={this.update('star_rating')}/>
+          <faStar className='star' size={100}/>
+        </label>
+        <label>
+          {/* <input type='text' value={this.state.star_rating} onChange={this.update('star_rating')} /> */}
+          <input type='radio' name='rating' value='2' onClick={this.update('star_rating')} />
+          <faStar className='star' size={100} />
+        </label>
+        <label>
+          {/* <input type='text' value={this.state.star_rating} onChange={this.update('star_rating')} /> */}
+          <input type='radio' name='rating' value='3' onClick={this.update('star_rating')} />
+          <faStar className='star' size={100} />
+        </label>
+        <label>
+          {/* <input type='text' value={this.state.star_rating} onChange={this.update('star_rating')} /> */}
+          <input type='radio' name='rating' value='4' onClick={this.update('star_rating')} />
+          <faStar className='star' size={100} />
+        </label>
+        <label>
+          {/* <input type='text' value={this.state.star_rating} onChange={this.update('star_rating')} /> */}
+          <input type='radio' name='rating' value='5' onClick={this.update('star_rating')} />
+          <faStar className='star' size={100} />
+        </label>
+        <label>
+          <input className='review-textbox' type='text' value={this.state.body} onChange={this.update('body')} style={{ top:'-100px'}} placeholder='Give back to the community. Share your thought about the trail so others know what to expect.'/>
         </label>
         <label>Activity Type
-          <input type='text' value={this.state.activity_type} onChange={this.update('activity_type')} />
+          <input type='textarea' value={this.state.activity_type} onChange={this.update('activity_type')} />
         </label>
         <label>Activity Date
           <input type='date' value={this.state.activity_date} onChange={this.update('activity_date')} />
-        </label>
-        <label>Review
-          <input type='text' value={this.state.body} onChange={this.update('body')} />
         </label>
         <input type='submit' value={this.props.formType} />
       </form>
